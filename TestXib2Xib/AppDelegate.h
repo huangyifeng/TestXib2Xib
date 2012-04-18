be8dbcbe193b51929ae37483bcbee373ae6ff1c9
huangyifeng <luckyJerry85@gmail.com>
2012-04-18 22:50:49 +0800

    Initial Commit

---
+++ TestXib2Xib/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestXib2Xib
+//
+//  Created by yifeng huang on 12-4-18.
+//  Copyright (c) 2012年 huangyifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestXib2Xib/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestXib2Xib
+//
+//  Created by yifeng huang on 12-4-18.
+//  Copyright (c) 2012年 huangyifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
